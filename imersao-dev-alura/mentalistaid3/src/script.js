c523223bb3df97f9e1ade896e1afef740e12eb50
Samara Alves <98350220+Samara-Alves@users.noreply.github.com>
2022-09-07 18:35:55 -0300

    Update script.js

--- imersao-dev-alura/mentalistaid3/src/script.js
+++ imersao-dev-alura/mentalistaid3/src/script.js
@@ -1,18 +1,17 @@
 //numero secreto sorteado
-
 var numeroSecreto = parseInt(Math.random() * 11);
-
 
 //elemento
 function Chutar() {
-   var tentativas = document.getElementById("tentativas");
+  var tentativas = document.getElementById("tentativas");
   var elementResultado = document.getElementById("resultado");
   var chute = parseInt(document.getElementById("valor").value);
   console.log(chute);
   if (chute == numeroSecreto) {
-    elementoResultado.innerHTML = "Parabéns, você acertou!!"
-  } elementoResultado.innerHTML = "Que pena, você errou."
+    elementResultado.innerHTML = "Parabéns, você acertou!!";
+  } else if (chute > 10 || chute < 0) {
+    elementResultado.innerHTML = "Que pena, você errou.";
   } else {
-   elementoResultado.innerHTML = "Errou, o número secreto era " + numeroSecreto;
+    elementResultado.innerHTML = "Errou, o número secreto era " + numeroSecreto;
   }
 }
