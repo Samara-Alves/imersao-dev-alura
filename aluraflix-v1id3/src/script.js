35a5255d72a6721f5397e3a7d95889a18bfbeb49
Samara Alves <98350220+Samara-Alves@users.noreply.github.com>
2022-09-10 18:24:28 -0300

    Add files via upload

---
+++ aluraflix-v1id3/src/script.js
@@ -0,0 +1,8 @@
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/b/b0/Avatar-Teaser-Poster.jpg/250px-Avatar-Teaser-Poster.jpg",
+  "https://br.web.img3.acsta.net/c_310_420/pictures/21/04/07/21/13/5564614.jpg",
+  "https://apostiladecinema.com.br/wp-content/uploads/2021/01/soul-poster-scaled.jpg"
+];
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
